6bd22e43a4662882a2b9e77675030a7d68d79dc5
ukatama <dev.ukatama@gmail.com>
2017-08-18 15:11:30 +0900

    Add DiceBot selector for browser example

--- examples/browser/src/index.js
+++ examples/browser/src/index.js
@@ -1,16 +1,22 @@
 import { newBcDice } from '../../../dist/bcdice';
 
-const bcDice = newBcDice('SwordWorld2_0');
-
+const diceBotElement = document.getElementById('diceBot');
 const inputElement = document.getElementById('input');
 const rollElement = document.getElementById('roll');
+const clearElement = document.getElementById('clear');
 const resultsElement = document.getElementById('results');
 
 rollElement.addEventListener('click', () => {
+    const bcDice = newBcDice(diceBotElement.value || null);
+
     bcDice.setMessage(inputElement.value);
     const result = bcDice.dice_command();
 
     const resultElement = document.createElement('p');
     resultElement.innerText = result[0];
     resultsElement.appendChild(resultElement);
+});
+
+clearElement.addEventListener('click', () => {
+    Array.prototype.map.call(resultsElement.children, (child) => resultsElement.removeChild(child));
 });
